37a2d81a99688b7ea4b1ada3aa29522e23cc8522
Urna1024 <296321889@qq.com>
2022-04-25 16:45:31 +0800

    首页获取轮播图和歌单

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,19 @@
 <template>
-    <SwiperCom></SwiperCom>
+    <SwiperCom/>
+    <IconList/>
+    <MusicList/>
+    
 </template>
 
 <script>
-// @ is an alias to /src
-
 import SwiperCom from "@/components/swiper/SwiperCom";
+import IconList from "@/components/icon-list/IconList";
+import MusicList from "@/components/music-list/MusicList";
+
 export default {
   name: 'HomeView',
   components: {
-      SwiperCom
+      SwiperCom,IconList,MusicList
     
   }
 }
